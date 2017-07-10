bf595e32a3b16d35bf02188d8d154ca5f9a156ce
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2017-07-10 17:19:42 -0600

    add landfalling location calc

--- centerline.py
+++ centerline.py
@@ -29,29 +29,45 @@ class Centerline():
 
         def least_cost(self, label_indices, ivt):
    
-                # Cost array creation. Set all values to -9999
-                cost_arr   = np.ones_like(ivt)*9999.999 
+            # Cost array creation. Set all values to -9999
+            cost_arr   = np.ones_like(ivt)*9999.999 
 
-                # Replace -9999 values of AR structure w/ cost
-                cost_arr[label_indices] = 5000.0 - ivt[label_indices]
+            # Replace -9999 values of AR structure w/ cost
+            cost_arr[label_indices] = 5000.0 - ivt[label_indices]
 
-                #start, end are tuples of indices (row,col)
-                nrow,ncol      = ivt.shape
-                indices, cost  = route_through_array(cost_arr, self.start, self.end)
-                indices        = np.array(indices).T
+            #start, end are tuples of indices (row,col)
+            nrow,ncol      = ivt.shape
+            indices, cost  = route_through_array(cost_arr, self.start, self.end)
+            indices        = np.array(indices).T
 
-                # When we index np arrays, we want a tuple of 1-d arrays
-                ind       = (indices[0], indices[1])
-                path      = np.zeros_like(cost_arr)
-                path[ind] = 1
-                path_len  = len(indices[0])
+            # When we index np arrays, we want a tuple of 1-d arrays
+            ind       = (indices[0], indices[1])
+            path      = np.zeros_like(cost_arr)
+            path[ind] = 1
+            path_len  = len(indices[0])
+                
+            self.path      = path
+            self.cost      = cost
+            self.path_len  = path_len
+            self.ind       = ind 
 
-                self.path      = path
-                self.cost      = cost
-                self.path_len  = path_len
-                self.ind       = ind 
+
+        def landfall_locator(self, land_mask):
+            p       = land_mask[self.ind]
+            p_diff  = np.diff(p)
+            p_where = np.argwhere(abs(p_diff) == 1)
+            self.p_where = p_where
+            try:
+                self.landfall_location = (self.ind[0][p_where[0][0]],self.ind[1][p_where[0][0]])
+                # returns a tuple of grid indices of landfall location
+
+            except:
+                self.landfall_location = None
+            
 
         # NOTE: Double Underscores
-        def __init__(self, label_indices_alt, label_indices, ivt):
+        def __init__(self, label_indices_alt, label_indices, ivt, land_mask):
                 self.distance_matrix(label_indices_alt)
                 self.least_cost(label_indices, ivt)
+                self.landfall_locator(land_mask)
+                self.label_indices = label_indices
